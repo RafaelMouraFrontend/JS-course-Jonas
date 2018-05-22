05e34389f24b20227a1ba39a4c7ce829c22ddb42
Rafael Moura <rafael.mouraweb@gmail.com>
2018-05-22 13:46:42 -0300

    fist commit lecture variables

---
+++ 2-JS-Basics/2-JS-Basics/main.js
@@ -0,0 +1,49 @@
+// Lecture 1: variables
+
+/*
+var name = 'Rafael';
+console.log(name);
+
+var lastName = 'Moura';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture: variables 2
+
+var name = 'Rafael';
+var age = 28;
+
+//console.log(name + age);
+//console.log(age + age);
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'teacher';
+isMarried = 'false';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried + '.');
+
+
+
+
+
